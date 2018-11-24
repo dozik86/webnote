5bd39f6d131dc58180a0e634c3bdde0d7b20577e
dozik86 <dozik86@gmail.com>
2018-11-25 00:14:41 +0300

    1.0

---
+++ src/main/java/com/dezox/webnote/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.dezox.webnote.repos;
+
+import com.dezox.webnote.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
