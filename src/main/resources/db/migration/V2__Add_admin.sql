f786124f6c3a3ba6797a2864f91bbc7ec45810f2
dozik86 <dozik86@gmail.com>
2018-11-26 03:19:45 +0300

    1.2

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
-insert into usr (id, username, password, active)
-values (1, 'admin', '123', true);
+insert into usr (id, username, password, active, email)
+values (1, 'admin', '$2a$08$dNSN4JxXQnq/mpCbkKqJyOEh//DVYDHliSS8Z7C4z3vevXJHR6Oiq', true, 'dozik86@gmail.com');
 
 insert into user_role (user_id, roles)
 values (1, 'USER'), (1, 'ADMIN');
